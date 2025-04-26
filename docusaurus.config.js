5f79de93ad540252f040d6b8d14ada3aa8ef8b82
Jtree03 <wowns0903@gmail.com>
2025-04-26 17:22:13 +0900

    Migration: docusaurus, react version upgrade

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,114 +1,137 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require("prism-react-renderer/themes/github");
-const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-    title: "조재준",
-    tagline: "JaeJun's blog site.",
-    url: "https://jaejun.me",
-    baseUrl: "/",
-    onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
-    favicon: "img/favicon.ico",
-    organizationName: "Jtree03",
-    projectName: "jaejun.me",
+	title: "조재준",
+	tagline: "JaeJun's blog site.",
+	url: "https://jaejun.me",
+	baseUrl: "/",
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
+	favicon: "img/favicon.ico",
+	organizationName: "Jtree03",
+	projectName: "jaejun.me",
 
-    presets: [
-        [
-            "classic",
-            /** @type {import('@docusaurus/preset-classic').Options} */
-            ({
-                docs: false,
-                blog: {
-                    showReadingTime: true,
-                    editUrl: "https://github.com/Jtree03/jaejun.me/blob/main",
-                    routeBasePath: "/",
-                },
-                theme: {
-                    customCss: require.resolve("./src/css/custom.css"),
-                },
-                gtag: { trackingID: "G-H34WTT5JJB" },
-            }),
-        ],
-    ],
+	presets: [
+		[
+			"classic",
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: false,
+				blog: {
+					showReadingTime: true,
+					editUrl: "https://github.com/Jtree03/jaejun.me/blob/main",
+					routeBasePath: "/",
+				},
+				theme: {
+					customCss: require.resolve("./src/css/custom.css"),
+				},
+				gtag: { trackingID: "G-H34WTT5JJB" },
+			}),
+		],
+	],
 
-    themeConfig:
-        /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-        ({
-            navbar: {
-                title: "JaeJun.me",
-                logo: {
-                    alt: "JaeJun.me Logo",
-                    src: "img/favicon.ico",
-                },
-                items: [
-                    // {
-                    //     to: "https://handsomely-boater-32d.notion.site/bd1205fe4362494193019279f33500f1",
-                    //     label: "About",
-                    //     position: "right",
-                    // },
-                    // {
-                    //   href: "https://github.com/Jtree03",
-                    //   label: "GitHub",
-                    //   position: "right",
-                    // },
-                ],
-            },
-            footer: {
-                style: "dark",
-                links: [
-                    {
-                        title: "Site",
-                        items: [
-                            {
-                                label: "Blog",
-                                to: "/",
-                            },
-                            // {
-                            //     label: "About",
-                            //     to: "https://handsomely-boater-32d.notion.site/bd1205fe4362494193019279f33500f1",
-                            // },
-                        ],
-                    },
-                    {
-                        title: "Email",
-                        items: [
-                            {
-                                label: "Gmail",
-                                href: "mailto:wowns0903@gmail.com",
-                            },
-                            {
-                                label: "Naver",
-                                href: "mailto:wowns0903@naver.com",
-                            },
-                        ],
-                    },
-                    {
-                        title: "More",
-                        items: [
-                            {
-                                label: "GitHub",
-                                href: "https://github.com/Jtree03",
-                            },
-                        ],
-                    },
-                ],
-                copyright: `Copyright © ${new Date().getFullYear()} JaeJun Jo, Inc. Built with Docusaurus.`,
-            },
-            prism: {
-                theme: lightCodeTheme,
-                darkTheme: darkCodeTheme,
-            },
-            googleAdsense: {
-                dataAdClient: "ca-pub-4869276592827991",
-            },
-            image: "img/og-image.png",
-        }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				title: "JaeJun.me",
+				logo: {
+					alt: "JaeJun.me Logo",
+					src: "img/favicon.ico",
+				},
+				items: [
+					// {
+					//     to: "https://handsomely-boater-32d.notion.site/bd1205fe4362494193019279f33500f1",
+					//     label: "About",
+					//     position: "right",
+					// },
+					{
+						href: "https://github.com/Jtree03",
+						label: "GitHub",
+						position: "right",
+					},
+				],
+			},
+			footer: {
+				style: "dark",
+				links: [
+					{
+						title: "Site",
+						items: [
+							{
+								label: "Blog",
+								to: "/",
+							},
+							// {
+							//     label: "About",
+							//     to: "https://handsomely-boater-32d.notion.site/bd1205fe4362494193019279f33500f1",
+							// },
+						],
+					},
+					{
+						title: "Email",
+						items: [
+							{
+								label: "Gmail",
+								href: "mailto:wowns0903@gmail.com",
+							},
+							{
+								label: "Naver",
+								href: "mailto:wowns0903@naver.com",
+							},
+						],
+					},
+					{
+						title: "More",
+						items: [
+							{
+								label: "GitHub",
+								href: "https://github.com/Jtree03",
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} JaeJun Jo, Inc. Built with Docusaurus.`,
+			},
+			image: "img/og-image.png",
+		}),
 
-    plugins: ["docusaurus-plugin-google-adsense"],
+	plugins: [
+		async function googleAdsensePlugin() {
+			const dataAdClient = "ca-pub-4869276592827991";
+
+			const isProd = process.env.NODE_ENV === "production";
+			return {
+				name: "google-adsense-plugin",
+				injectHtmlTags() {
+					if (!isProd) {
+						return {};
+					}
+					return {
+						headTags: [
+							{
+								tagName: "link",
+								attributes: {
+									rel: "preconnect",
+									href: "https://pagead2.googlesyndication.com",
+								},
+							},
+							{
+								tagName: "script",
+								attributes: {
+									async: true,
+									src: `https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${dataAdClient}`,
+									crossorigin: "anonymous",
+								},
+							},
+						],
+					};
+				},
+			};
+		},
+	],
 };
 
 module.exports = config;
--- src/theme/BlogPostItem.js
+++ src/theme/BlogPostItem.js
@@ -1,53 +1,53 @@
 import React, { useEffect, useRef } from "react";
 import OriginalBlogPostItem from "@theme-original/BlogPostItem";
 import { useThemeConfig } from "@docusaurus/theme-common";
-import { useBlogPost } from "@docusaurus/theme-common/internal";
+import { useBlogPost } from "@docusaurus/plugin-content-blog/client";
 
 function BlogPostItem(props) {
-  const { isDarkTheme } = useThemeConfig();
-  const { isBlogPostPage } = useBlogPost();
-  const utterancesTheme = isDarkTheme ? "github-dark" : "github-light";
-  const containerRef = useRef(null);
+	const { isDarkTheme } = useThemeConfig();
+	const { isBlogPostPage } = useBlogPost();
+	const utterancesTheme = isDarkTheme ? "github-dark" : "github-light";
+	const containerRef = useRef(null);
 
-  useEffect(() => {
-    if (!isBlogPostPage) return;
+	useEffect(() => {
+		if (!isBlogPostPage) return;
 
-    const utterancesEl = containerRef.current.querySelector(
-      "iframe.utterances-frame"
-    );
+		const utterancesEl = containerRef.current.querySelector(
+			"iframe.utterances-frame",
+		);
 
-    const createUtterancesEl = () => {
-      const script = document.createElement("script");
+		const createUtterancesEl = () => {
+			const script = document.createElement("script");
 
-      script.src = "https://utteranc.es/client.js";
-      script.setAttribute("repo", "jtree03/jaejun.me");
-      script.setAttribute("issue-term", "pathname");
-      script.setAttribute("label", "blog_comment");
-      script.setAttribute("theme", utterancesTheme);
-      script.crossOrigin = "anonymous";
-      script.async = true;
+			script.src = "https://utteranc.es/client.js";
+			script.setAttribute("repo", "jtree03/jaejun.me");
+			script.setAttribute("issue-term", "pathname");
+			script.setAttribute("label", "blog_comment");
+			script.setAttribute("theme", utterancesTheme);
+			script.crossOrigin = "anonymous";
+			script.async = true;
 
-      containerRef.current.appendChild(script);
-    };
+			containerRef.current.appendChild(script);
+		};
 
-    const postThemeMessage = () => {
-      const message = {
-        type: "set-theme",
-        theme: utterancesTheme,
-      };
+		const postThemeMessage = () => {
+			const message = {
+				type: "set-theme",
+				theme: utterancesTheme,
+			};
 
-      utterancesEl.contentWindow.postMessage(message, "https://utteranc.es");
-    };
+			utterancesEl.contentWindow.postMessage(message, "https://utteranc.es");
+		};
 
-    utterancesEl ? postThemeMessage() : createUtterancesEl();
-  }, [utterancesTheme]);
+		utterancesEl ? postThemeMessage() : createUtterancesEl();
+	}, [isBlogPostPage, utterancesTheme]);
 
-  return (
-    <>
-      <OriginalBlogPostItem {...props} />
-      {isBlogPostPage && <div ref={containerRef} />}
-    </>
-  );
+	return (
+		<>
+			<OriginalBlogPostItem {...props} />
+			{isBlogPostPage && <div ref={containerRef} />}
+		</>
+	);
 }
 
 export default BlogPostItem;
